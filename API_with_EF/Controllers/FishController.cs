cceed6ccdbda3bbf6e027f68dc989c13738d9129
craig <craig@ten987.com>
2023-02-20 20:58:15 -0500

    Added Visual Studio project

---
+++ API_with_EF/Controllers/FishController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API_with_EF.Models;
+using API_with_EF.DAL;
+
+namespace API_with_EF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FishController : ControllerBase
+    {
+        FishRepository repo = new FishRepository();
+        [HttpPost("spawn")]
+        public Fish AddFish(string name, string classification, bool fresh, bool salt)
+        {
+            Fish newFish = new Fish()
+            {
+                Name = name,
+                Classification = classification,
+                Saltwater = salt,
+                FreshWater = fresh
+            };
+
+            return repo.AddFish(newFish);
+        }
+    }
+}
